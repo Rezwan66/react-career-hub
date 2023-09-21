df58d283b0a35d104aa0958e10e0a9583ccda399
Rezwan66 <shaikhrezwan66@gmail.com>
2023-09-22 02:47:28 +0600

    first commit- react router, tailwind, daisyUI setup

---
+++ src/components/Root/Root.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from 'react-router-dom';
+
+const Root = () => {
+  return (
+    <div>
+      <div>Hello world!</div>
+      <Outlet></Outlet>
+    </div>
+  );
+};
+
+export default Root;
